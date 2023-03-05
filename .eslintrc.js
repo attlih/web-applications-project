90b144044be7a69fcef2d6f18e6df32a166e6456
attlih <98050742+attlih@users.noreply.github.com>
2023-03-05 15:53:43 +0200

    change eslint.js location

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./client/tsconfig.json']
+    project: ['./tsconfig.json']
   },
   plugins: [
     'react'
